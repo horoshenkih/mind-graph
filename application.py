8e2f23319ac0deb92ede3d8bf99f913aa55cc367
horoshenkih <horoshenkih91@gmail.com>
2017-05-04 08:13:31 +0300

    aws

--- application.py
+++ application.py
@@ -4,19 +4,20 @@ from flask import Flask, render_template, request, jsonify
 
 from organon.parser import Parser
 
-app = Flask(__name__)
+application = Flask(__name__)
 ps = Parser()
 
 
-@app.route('/')
+@application.route('/')
+@application.route('/index')
 def index(name=None):
     return render_template('index.html')
 
 
-@app.route('/get_graph')
+@application.route('/get_graph')
 def get_graph(methods=["GET"]):
     text = request.args.get('input_text', "", type=str)
-    app.logger.info("GOT: " + text)
+    application.logger.info("GOT: " + text)
     repr = ps.parse_to_list(text)
 
     return jsonify(repr=repr)
@@ -24,4 +25,4 @@ def get_graph(methods=["GET"]):
 
 # run locally
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8080, debug=True)
+    application.run()
