6721f77545ae8ccc5af53fd191709410eb2dea9c
horoshenkih <horoshenkih91@gmail.com>
2017-06-05 12:03:45 +0300

    implement base functionality of Storage

--- static/js/lib/storage.js
+++ static/js/lib/storage.js
@@ -2,66 +2,143 @@
  * Created by khoroshenkikh on 01.06.17.
  */
 
+function normalizePath(path) {
+    return path.replace(/\/+/g, '/');
+}
+
 // prototypes
-function BaseFile() {
+function BaseFile(path, content) {
     return {
-        name: undefined,
-        parentDirectory: undefined,
-        content: undefined,
-        read: function () {
-            return this.content;
-        },
-        write: function (newContent) {
-            this.content = newContent;
-        }
+        _type: 'file',
+        path: path,
+        content: content
     };
 }
 
-// var Directory = undefined;
+function BaseDirectory(path, contentNames) {
+    return {
+        _type: 'directory',
+        path: path,
+        contentNames: contentNames
+    }
+}
+
 function BaseStorage() {
     return {
         name: undefined,
-            accessStorage: function () {},
+        accessStorage: function () {},
 
-        createDirectory: function (currentDirectory) {},
-        listDirectory: function (currentDirectory) {},
+        createDirectory: function (directory) {},
+        listDirectory: function (directory) {},
 
-        createFile: function (directory, filename) {}
+        createFile: function (filePath) {},
+        readFile: function (filePath) {},
+        writeFile: function (filePath) {}
     };
 }
 
 // implementations
+///////////////////////////////////
+var EmptyFile = new BaseFile(undefined, "ko .is framework\nko {http://knockoutjs.com/}");
+// var RootDirectory = new BaseDirectory('/', undefined);
+
 var EmptyStorage = new BaseStorage();
 EmptyStorage.accessStorage = undefined;
 EmptyStorage.createDirectory = undefined;
 EmptyStorage.listDirectory = undefined;
 EmptyStorage.createFile = undefined;
 
+//////////////////////////////////////
 var ExampleStorage = new BaseStorage();
 ExampleStorage.name = "Examples";
-ExampleStorage.createDirectory = undefined;
-ExampleStorage.listDirectory = function (currentDirectory) {
-    if (currentDirectory !== 'examples_directory') {
-        return [];
-    }
-    files = [];
-    for (name in this.examples_directory) {
-        var f = new BaseFile();
-        f.name = name;
-        f.parentDirectory = currentDirectory;
-        f.content = this.examples_directory[name];
-        files.push(f);
-    }
-    return files;
-};
-ExampleStorage.createFile = undefined;
-ExampleStorage.readFile = undefined;
-ExampleStorage.writeFile = undefined;
 
 // data
-ExampleStorage.examples_directory = {
-    basic: "a, b .is c"
+ExampleStorage._examples = {
+    "Basic examples": {
+        1: "a, b .is c"
+    },
+    "Advanced examples": {
+        1: "a {\
+        # Markdown header\
+        }\
+        a .is b"
+    }
 };
+
+
+ExampleStorage._files = {};  // path -> object
+ExampleStorage._accessed = false;
+
+ExampleStorage.accessStorage = function () {
+    var self = this;
+
+    var fringe = [[]];
+
+    // traverse
+    while(fringe.length) {
+        var pathToTraverse = fringe.shift();
+
+        var currentContent = self._examples;
+        for (i in pathToTraverse) {
+            var key_name = pathToTraverse[i];
+            currentContent = currentContent[key_name];
+        }
+
+        var pathStr = '/' + pathToTraverse.join('/');
+        if (typeof currentContent === 'string') {
+            // file
+            self._files[pathStr] = new BaseFile(pathStr, currentContent);
+        } else if (typeof currentContent === 'object') {
+            // directory
+            self._files[pathStr] = new BaseDirectory(pathStr, Object.keys(currentContent));
+            for (j in currentContent) {
+                var currentPath = pathToTraverse.concat([j]);
+                var currentPathStr = '/' + currentPath.join('/');
+                var value = currentContent[j];
+
+                self._files[currentPathStr] = new BaseDirectory(currentPathStr, value);
+                // need to go deeper
+                fringe.push(currentPath);
+            }
+        }
+    }
+    self._accessed = true;
+};
+
+ExampleStorage.listDirectory = function (directoryPath) {
+    if (!this._accessed) {
+        this.accessStorage();
+    }
+    directoryPath = normalizePath(directoryPath);
+    var storageItem = this._files[directoryPath];
+    var content = [];
+    if (storageItem && storageItem._type === 'directory') {
+        for (i in storageItem.contentNames) {
+            var subItem = storageItem.contentNames[i];
+            var subItemPath = normalizePath(directoryPath + '/' + subItem);
+            var subItemFile = this._files[subItemPath];
+            if (subItemFile) {
+                content.push(subItemFile);
+            }
+        }
+    }
+    return content;
+};
+
+ExampleStorage.readFile = function (filePath) {
+    if (!this._accessed) {
+        this.accessStorage();
+    }
+    filePath = normalizePath(filePath);
+    var storageItem = this._files[filePath];
+    if (storageItem && storageItem._type === 'file') {
+        return storageItem.content;
+    }
+};
+
+ExampleStorage.createDirectory = undefined;
+ExampleStorage.createFile = undefined;
+ExampleStorage.writeFile = undefined;
 
 var storages = [
     ExampleStorage
--- static/js/lib/vm.js
+++ static/js/lib/vm.js
@@ -4,7 +4,46 @@
 var applyViewModel = function() {
     var self = this;
 
-    self.graphText = ko.observable("ko .is framework\nko {http://knockoutjs.com/}");
+    self.storages = storages;
+    self.selectedStorage = ko.observable(EmptyStorage);
+    self.selectedDirectoryPath = ko.observable('/');
+    self.selectedFilePath = ko.observable('');
+
+    self.selectStorage = function (data, event) {
+        self.selectedStorage(data);
+        self.selectedStorage().accessStorage();
+    };
+
+    self.isActiveSaveButton = ko.computed(function () {
+        return !(self.selectedStorage().createFile === undefined);
+    });
+
+    self.isActiveOpenButton = ko.computed(function () {
+        return !(self.selectedStorage().accessStorage === undefined);
+    });
+
+    self.listDirectory = ko.computed(function () {
+        var st = self.selectedStorage();
+        if (st.listDirectory) {
+            return st.listDirectory(self.selectedDirectoryPath());
+        }
+    });
+
+    self.selectInDirectory = function (data, event) {
+        if (data._type === 'file') {
+            self.selectedFilePath(data.path);
+        } else if (data._type === 'directory') {
+            self.selectedDirectoryPath(data.path);
+        }
+    };
+
+    // self.graphText = ko.observable("ko .is framework\nko {http://knockoutjs.com/}");
+    self.graphText = ko.computed(function () {
+        var st = self.selectedStorage();
+        if (st.readFile) {
+            return st.readFile(self.selectedFilePath());
+        }
+    });
 
     self.graph = ko.computed(function () {
         var input_text = self.graphText();
@@ -20,21 +59,6 @@ var applyViewModel = function() {
     self.updateNodeInfo = function () {
         if (self.graph) { document.getElementById('nodeInfo').innerHTML = self.graph.getSelectedNodeInfo() }
     };
-
-    self.storages = storages;
-    self.selectedStorage = ko.observable(EmptyStorage);
-
-    self.selectStorage = function (data, event) {
-        self.selectedStorage(data);
-    };
-
-    self.isActiveSaveButton = ko.computed(function () {
-        return !(self.selectedStorage().createFile === undefined);
-    });
-
-    self.isActiveOpenButton = ko.computed(function () {
-        return !(self.selectedStorage().accessStorage === undefined);
-    });
 };
 
 ko.applyBindings(new applyViewModel());
